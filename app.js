9535860e01d3976927031fd4a92f5c7109f67d7b
NinoGveno <nino.gvenetadze1@gmail.com>
2023-05-25 20:53:33 +0400

    first section html css js

--- app.js
+++ app.js
@@ -12,3 +12,19 @@ setInterval(() => {
   showSlide(currentSlide + 1);
 }, 5000);
 
+window.addEventListener('scroll', function() {
+    var skillBar = document.querySelector('.skill-bar');
+    var skillBarFill = document.querySelectorAll('.skill-bar-fill');
+    var skillBarPosition = skillBar.getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+  
+    for (var i = 0; i < skillBarFill.length; i++) {
+      var skillBarFillPercentage = skillBarFill[i].getAttribute('data-percentage');
+      var skillBarFillWidth = (skillBarFillPercentage / 100) * skillBar.offsetWidth;
+  
+      if (skillBarPosition < windowHeight) {
+        skillBarFill[i].style.width = skillBarFillWidth + 'px';
+      }
+    }
+  });
+ 
